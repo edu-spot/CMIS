852a413225ce6429e0d5ce04f84b7ae973eff6fe
Sushrut <sushrutdhadwe@gmail.com>
2017-12-25 01:25:14 +0530

    added authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
             $table->string('password');
+            $table->string('email')->unique();
+            $table->char('user_type')->default('S');
+            $table->tinyInteger('is_block')->default('0');
+            $table->integer('created_by')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
