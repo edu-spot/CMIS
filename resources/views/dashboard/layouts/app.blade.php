a2b64ec0e58d0599f97a4c04f30944742c61652a
swapnildabade24 <swapnildabade24@gmail.com>
2018-01-28 02:20:15 +0530

    Dashboard Template(Admin Lte) added

---
+++ resources/views/dashboard/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	@include('dashboard.layouts.head')
+</head>
+
+<body>
+	<body class="hold-transition skin-purple sidebar-mini">
+	<div class="wrapper">
+
+		@include('dashboard.layouts.header')
+		@include('dashboard.layouts.sidebar')
+		@include('dashboard.layouts.blank')
+		@include('dashboard.layouts.footer')
+
+	</div>
+	
+</body>
+
+</html>
