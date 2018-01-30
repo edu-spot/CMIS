5ebbd82a4f8aeb6f29f36419ed3d5c80e4ac9dd9
yash patil <yashpatil516@gmail.com>
2018-01-30 15:53:18 +0530

    Added temporary Layouts to all kinds of user(Not linked anything)

---
+++ resources/views/Student/layouts/footer.blade.php
@@ -0,0 +1,45 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.0
+    </div>
+    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    reserved.
+  </footer>
+
+  <!-- jQuery 3 -->
+<script src="{{asset('dashboard/bower_components/jquery/dist/jquery.min.js') }}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('dashboard/bower_components/jquery-ui/jquery-ui.min.js') }}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{asset('dashboard/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!-- Morris.js charts -->
+<script src="{{asset('dashboard/bower_components/raphael/raphael.min.js') }}"></script>
+<script src="{{asset('dashboard/bower_components/morris.js/morris.min.js') }}"></script>
+<!-- Sparkline -->
+<script src="{{asset('dashboard/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') }}"></script>
+<!-- jvectormap -->
+<script src="{{asset('dashboard/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+<script src="{{asset('dashboard/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{asset('dashboard/bower_components/jquery-knob/dist/jquery.knob.min.js') }}"></script>
+<!-- daterangepicker -->
+<script src="{{asset('dashboard/bower_components/moment/min/moment.min.js') }}"></script>
+<script src="{{asset('dashboard/bower_components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+<!-- datepicker -->
+<script src="{{asset('dashboard/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="{{asset('dashboard/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
+<!-- Slimscroll -->
+<script src="{{asset('dashboard/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+<!-- FastClick -->
+<script src="{{asset('dashboard/bower_components/fastclick/lib/fastclick.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('dashboard/dist/js/adminlte.min.js') }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('dashboard/dist/js/pages/dashboard.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('dashboard/dist/js/demo.js') }}"></script>
---
+++ resources/views/Tpo/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	@include('admin.layouts.head')
+</head>
+
+<body>
+	<body class="hold-transition skin-purple sidebar-mini">
+	<div class="wrapper">
+
+		@include('admin.layouts.header')
+		@include('admin.layouts.sidebar')
+		@include('admin.layouts.blank')
+		@include('admin.layouts.footer')
+
+	</div>
+	
+</body>
+
+</html>
