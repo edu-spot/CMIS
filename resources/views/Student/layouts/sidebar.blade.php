372415e5550f7ce335b8f085827b24f75f8b87ed
yash patil <yashpatil516@gmail.com>
2018-01-30 20:38:28 +0530

    Student Homepage Sidebar Modified

--- resources/views/Student/layouts/sidebar.blade.php
+++ resources/views/Student/layouts/sidebar.blade.php
@@ -33,24 +33,95 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Profile</a></li>
           </ul>
         </li>
         
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-edit"></i> <span>Student Management</span>
+            <i class="fa fa-edit"></i> <span>Notice Board</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="/add"><i class="fa fa-circle-o"></i> Add Student</a></li>
-            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Import Students</a></li>
-            <li><a href="pages/forms/editors.html"><i class="fa fa-circle-o"></i> Manage Students</a></li>
+            <li><a href="/add"><i class="fa fa-circle-o"></i> Recent Notices</a></li>
+            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Search Notices</a></li>
           </ul>
         </li>
+
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Attendance/span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="/add"><i class="fa fa-circle-o"></i> Attendance Report</a></li>
+            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Defaulter List</a></li>
+            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Request for Leave</a></li>
+          </ul>
+        </li>
+
+
+          <ul class="treeview-menu">
+            <li><a href="index.html"><i class="fa fa-circle-o"></i> Timetable</a></li>
+          </ul>
+
+          <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Fees Payment</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="/add"><i class="fa fa-circle-o"></i> Pay Fees Online</a></li>
+            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Fees Report</a></li>
+          </ul>
+        </li>
+ 
+          <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Calendar</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="/add"><i class="fa fa-circle-o"></i> Add Event</a></li>
+            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Manage Event</a></li>
+          </ul>
+        </li>
+
+          <ul class="treeview-menu">
+            <li><a href="index.html"><i class="fa fa-circle-o"></i> Upload Documents</a></li>
+          </ul>
+
+          <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span> Feedback</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="/add"><i class="fa fa-circle-o"></i> New Feedback</a></li>
+            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Check Feedback</a></li>
+            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Complaints And Suggestions</a></li>
+          </ul>
+        </li>
+
+          <ul class="treeview-menu">
+            <li><a href="index.html"><i class="fa fa-circle-o"></i> Downloads</a></li>
+          </ul>
+
+          <ul class="treeview-menu">
+            <li><a href="index.html"><i class="fa fa-circle-o"></i> Help</a></li>
+          </ul>
+
         
       </ul>
     </section>
